e9374932489c3ff64eef4a29256785be7abdfdb0
Miloš Petrović <milosptr@hotmail.com>
2019-01-11 17:49:41 +0100

    build project

---
+++ build/precache-manifest.cd580804d936a62d9e77200fa90930d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d44e739101acefb76e8884f268eda5a8",
+    "url": "/static/media/user.d44e7391.svg"
+  },
+  {
+    "revision": "7b365e7dc6552c310aac",
+    "url": "/static/css/main.4cb6b08d.chunk.css"
+  },
+  {
+    "revision": "e4b84c4b7d2f29f385708e5797a8654d",
+    "url": "/static/media/abstract-background.e4b84c4b.jpg"
+  },
+  {
+    "revision": "ff1c6c3ada353103447d",
+    "url": "/static/js/1.ff1c6c3a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b365e7dc6552c310aac",
+    "url": "/static/js/main.7b365e7d.chunk.js"
+  },
+  {
+    "revision": "bb67460ec64b52eae3ced868288bec43",
+    "url": "/static/media/pass.bb67460e.svg"
+  },
+  {
+    "revision": "07f6a524ff0d0bfe7da5d81264a350b2",
+    "url": "/static/media/logo.07f6a524.png"
+  },
+  {
+    "revision": "44fdce44431344362308ad6766e6f0f6",
+    "url": "/static/media/login-side-image.44fdce44.jpg"
+  },
+  {
+    "revision": "ff1c6c3ada353103447d",
+    "url": "/static/css/1.3731170a.chunk.css"
+  },
+  {
+    "revision": "35c8a4a7c419bf5e0b6aa19ee54523d0",
+    "url": "/index.html"
+  }
+];
